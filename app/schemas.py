9250e5eeb08d0d8f42714458d81af6b2f7cbe28e
Bogdan Mijovic <bogdanmijovic@gmail.com>
2026-08-15 15:08:48 +0200

    feat: add shcemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,18 +1,30 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import Optional
+from app.models import UserBase
 
 class TicketCreate(BaseModel):
     title: str
     description: str
-    resolution: Optional[str] = None
+
+class UserCreate(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    password: str
 
 class TicketUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
     resolution: Optional[str] = None
 
+class UserOut(UserBase):
+    pass
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
 class Token(BaseModel):
-    token: str
-    type: str
+    access_token: str
+    token_type: str
 
 
